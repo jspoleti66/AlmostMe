8144ccbbbf8af8907d1f6f19cadfc56d526b2e36
jspoleti66 <jspoletti@yahoo.com.ar>
2026-01-12 16:45:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def consultar_github(historial):
                 mensajes.append(AssistantMessage(content=msg["content"]))
 
         response = client.complete(
-            model="Llama-4-Scout-17B-16E-Instruct",
+            model="Meta-Llama-3.1-8B-Instruct",
             messages=mensajes,
             temperature=0.25,
             max_tokens=512,
@@ -154,6 +154,7 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
