7717b80dd0c3ade9c02218b0877c2a758a59179e
jspoleti66 <jspoletti@yahoo.com.ar>
2026-01-12 12:00:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def consultar_github(historial):
 
         # PETICIÓN
         response = client.complete(
-            model="Llama-3.1-8B-Instruct",
+            model="Llama-3.3-70B-Instruct",
             messages=mensajes,
             temperature=0.25,
             max_tokens=1024,
@@ -143,4 +143,5 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
