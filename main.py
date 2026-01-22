8fe1a47f8e5b14d4bcb62fedbff741273ff34c38
jspoleti66 <jspoletti@yahoo.com.ar>
2026-01-22 13:30:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import json
 from flask import Flask, request, jsonify, render_template, session
 from datetime import timedelta
-from github import GithubModels  # SDK GitHub Models
 
 # =====================================================
 # APP
@@ -112,3 +111,4 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
